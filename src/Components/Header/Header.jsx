9e63596dc05acab8b732b4b6ae16c8fc91ebc93f
rjspyk5 <rjspyk5@gmail.com>
2024-03-30 02:59:44 +0600

    react router install

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export const Header = () => {
   return (
     <div>
-        
-        <h1 className='text-3xl font-black text-center py-8'>Bottle Er Dokan</h1>
+      <h1 className='text-3xl font-black text-center py-8'>Bottle Er Dokan</h1>
     </div>
-  )
-}
+  );
+};
