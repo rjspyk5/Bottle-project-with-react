f5d697d1337c7b27f11bd4c324e04b7e5178e529
rjspyk5 <rjspyk5@gmail.com>
2024-03-30 14:05:10 +0600

    Pages created

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,6 @@ export const Header = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <h1 className='text-3xl font-black text-center py-8'>Bottle Er Dokan</h1>
     </div>
   );
 };
--- src/Components/Routing/Routing.jsx
+++ src/Components/Routing/Routing.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Main } from '../Main/Main';
 import { Start } from '../Start/Start';
 import { Login } from '../Login/Login';
+import { Services } from '../Pages/Services/Services';
+import { About } from '../Pages/About/About';
+import { Contact } from '../Pages/Contact/Contact';
+import { MyCart } from '../Pages/MyCart/MyCart';
+
 export const router = createBrowserRouter([
   {
     path: '/',
@@ -12,6 +17,10 @@ export const router = createBrowserRouter([
         path: '/login',
         element: <Login />,
       },
+      { path: '/service', element: <Services /> },
+      { path: '/about', element: <About /> },
+      { path: '/contact', element: <Contact /> },
+      { path: '/cart', element: <MyCart /> },
     ],
   },
 ]);
