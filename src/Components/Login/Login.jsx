44a06730a13af1f2085896822078deedc784fb44
rjspyk5 <rjspyk5@gmail.com>
2024-03-30 14:22:04 +0600

    simple login form create

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -14,6 +14,38 @@ export const Login = () => {
   };
   return (
     <div>
+      <div className='flex justify-center max-w-1/2'>
+        <form className='space-y-4 border p-2 rounded-lg '>
+          <legend className='text-center'>Login Form</legend>
+          <label htmlFor='email'> Email :</label>
+          <input
+            className='bg-slate-300 p-1 rounded-lg'
+            name='email'
+            type='text'
+          />{' '}
+          <br />
+          <label htmlFor='pass'> Password :</label>
+          <input
+            className='bg-slate-300 p-1 rounded-lg'
+            name='pass'
+            type='text'
+          />
+          <br />
+          <input
+            type='submit'
+            className='bg-green-500 text-[white] p-1 w-full rounded-md'
+            value='Submit'
+          />
+          <div className='flex space-x-4'>
+            <button className='bg-green-500 text-[white] p-1 rounded-md'>
+              Login with Google
+            </button>
+            <button className='bg-green-500 text-[white] p-1 rounded-md'>
+              Login with github
+            </button>
+          </div>
+        </form>
+      </div>
       <button onClick={handleClick}>Google Login</button>
     </div>
   );
