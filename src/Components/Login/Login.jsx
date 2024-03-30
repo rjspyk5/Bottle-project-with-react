573b49e3379b1b8a686f05c9bf37876802c34e78
rjspyk5 <rjspyk5@gmail.com>
2024-03-30 15:12:22 +0600

    login and signout functionality added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,17 +1,31 @@
-import { getAuth, signInWithPopup } from 'firebase/auth';
+import { getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import { app } from '../Firebase/firebase.init';
 import { GoogleAuthProvider } from 'firebase/auth';
+import { FaGoogle } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { useState } from 'react';
 
 export const Login = () => {
+  const [user, setuser] = useState({});
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  const handleClick = () => {
+  const handleClick = e => {
+    e.preventDefault();
+    console.log('clicked');
     signInWithPopup(auth, provider)
-      .then(res => {
-        console.log(res);
-      })
+      .then(res => setuser(res.user))
       .catch(er => console.log(er));
   };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('signOut success');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  console.log(user);
   return (
     <div>
       <div className='flex justify-center max-w-1/2'>
@@ -19,7 +33,7 @@ export const Login = () => {
           <legend className='text-center'>Login Form</legend>
           <label htmlFor='email'> Email :</label>
           <input
-            className='bg-slate-300 p-1 rounded-lg'
+            className='bg-slate-300 p-1 rounded-lg ml-[28px]'
             name='email'
             type='text'
           />{' '}
@@ -37,16 +51,36 @@ export const Login = () => {
             value='Submit'
           />
           <div className='flex space-x-4'>
-            <button className='bg-green-500 text-[white] p-1 rounded-md'>
-              Login with Google
-            </button>
-            <button className='bg-green-500 text-[white] p-1 rounded-md'>
-              Login with github
+            {user ? (
+              <>
+                <button
+                  onClick={handleClick}
+                  className='bg-gray-400 btn text-[white] p-1 flex items-center space-x-3 rounded-md'
+                >
+                  <span> Login with Google </span>
+                  <FaGoogle />
+                </button>
+                <button className='btn bg-gray-400 text-[white] p-1 flex items-center space-x-3 rounded-md'>
+                  <span>Login with github</span> <FaGithub />
+                </button>
+              </>
+            ) : (
+              <button
+                onClick={handleSignOut}
+                className='btn bg-gray-400 text-[white] p-1 flex items-center space-x-3 rounded-md'
+              >
+                SignOut
+              </button>
+            )}
+            <button
+              onClick={handleSignOut}
+              className='btn bg-gray-400 text-[white] p-1 flex items-center space-x-3 rounded-md'
+            >
+              SignOut
             </button>
           </div>
         </form>
       </div>
-      <button onClick={handleClick}>Google Login</button>
     </div>
   );
 };
